28bb9db5f228153517a0265f1449ac7c452569e2
Marvin Pohl <marvin@lab132.com>
2024-09-16 10:45:19 +0200

    Enhance form rendering with customizable submit button text and add action callback support in table rendering. Introduce new macro for rendering datagrid items.

--- flask_tabler/forms.py
+++ flask_tabler/forms.py
@@ -1,7 +1,7 @@
 from dominate.tags import *
 
 
-def render_form(render_form, title, action, method="POST"):
+def render_form(render_form, title, action, method="POST", submit_text="Submit"):
     result_form = form(_class="card", action=action, method=method)
     with result_form:
         for error in render_form.errors:
@@ -19,17 +19,17 @@ def render_form(render_form, title, action, method="POST"):
                 else:
                     with div(_class="mb-3"):
                         div(str(field.label), _class="form-label")
-                        input_field = input_(_class="form-control", type=field.type, name=field.name, id=field.id, value=field.default)
+                        input_field = input_(_class="form-control", type=field.type, name=field.name, id=field.id, value=field.data)
                         if render_form.errors and field.name in render_form.errors:
                             input_field["class"] += " is-invalid"
                             with div(_class="invalid-feedback"):
                                 div(render_form.errors[field.name])
         with div(_class="card-footer text-end"):
-            button("Submit", _class="btn btn-primary", type="submit", value="Submit")
+            button(submit_text, _class="btn btn-primary", type="submit", value=submit_text)
 
     return result_form
 
-def render_table(objects, headers):
+def render_table(objects, headers, action_cb=None):
     container = div(_class="card")
     with container:
         with div(_class="table-responsive"):
@@ -38,10 +38,15 @@ def render_table(objects, headers):
                     with tr():
                         for header in headers:
                             th(header)
+                        if action_cb:
+                            th(_class="w-1")
                 with tbody():
                     for obj in objects:
                         with tr():
                             for header in headers:
                                 td(obj[header])
+                            if action_cb:
+                                with td() as action_col:
+                                    action_cb(action_col, obj)
 
     return container
